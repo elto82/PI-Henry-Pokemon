2977b5c18cec5de88fbbc7f38f4c856ac738dcc5
Argiro <elto.82@gmai.com>
2023-07-24 15:58:16 -0500

    url

--- api/src/app.js
+++ api/src/app.js
@@ -14,10 +14,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://deploy-five-sigma.vercel.app"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://poke-a-p-p.vercel.app/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
